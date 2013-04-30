d1a9b352aefdfe7f6cebee6bb46f3274f2972628
Philipp Walker <root@trixbox1.localdomain>
2013-04-30 11:20:55 +0200

    Very basic m9 support

---
+++ endpoint/snom/m9/phone.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Snom m9 Provisioning System
+ *
+ * @author Andrew Nagy & Jort
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_snom_m9_phone extends endpoint_snom_base {
+
+	public $family_line = 'm9';
+	
+}
